62703b577e34be5f254724457f90a3825e296468
unishigure <s.reiuniuni@gmail.com>
2023-08-16 01:53:24 +0900

    Add usage dialogue

---
+++ app/component/howTo.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import Link from "@mui/material/Link";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function HowToDialog() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        {"Usage"}
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"How to add Google Calender events"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <ol className="list-decimal pl-4 pr-4">
+              <li>{"CSVファイルを新規作成する"}</li>
+              <li>
+                <b>{"Header Copy"}</b>
+                {"ボタンを押下し、ヘッダーをクリップボードにコピー"}
+              </li>
+              <li>{"新規作成したCSVファイルの1行目にコピーする"}</li>
+              <li>{"イベント内容を入力する"}</li>
+              <li>
+                <b>{"Event Copy"}</b>
+                {"ボタンを押下し、クリップボードにコピー"}
+              </li>
+              <li>{"新規作成したCSVファイルの2行目以降にコピーする"}</li>
+              <li>{"上記4~6を、追加したい予定の数だけ繰り返す"}</li>
+              <li>
+                {"作成したCSVファイルを"}
+                <Link href="https://calendar.google.com/" underline="hover">
+                  {"Googleカレンダー"}
+                </Link>
+                {"にインポートする"}
+              </li>
+            </ol>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            {"OK"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { GridRowsProp } from "@mui/x-data-grid";
 import ScheduleForm from "./component/scheduleForm";
 import ScheduleTable from "./component/scheduleTable";
+import HowToDialog from "./component/howTo";
 
 let scheduleRows: Array<Object> = [];
 
@@ -8,10 +9,9 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center p-8 bg-slate-400 text-black/60">
       <h1 className="text-3xl mb-4">gCal&nbsp;CSV&nbsp;Generator</h1>
-      <div>
-        <ScheduleForm {...scheduleRows} />
-        {/* <ScheduleTable /> */}
-      </div>
+      <HowToDialog />
+      <ScheduleForm {...scheduleRows} />
+      {/* <ScheduleTable /> */}
     </main>
   );
 }
