8ef923f5f349972d74f64d89a9d33bdec0bfbb0c
unishigure <s.reiuniuni@gmail.com>
2023-08-15 19:00:44 +0900

    Add form function

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import { GridRowsProp } from "@mui/x-data-grid";
 import ScheduleForm from "./component/scheduleForm";
 import ScheduleTable from "./component/scheduleTable";
+
+let scheduleRows: Array<Object> = [];
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center p-8 bg-slate-400 text-black/60">
       <h1 className="text-3xl mb-4">gCal&nbsp;CSV&nbsp;Generator</h1>
       <div>
-        {/* <ScheduleForm /> */}
-        <ScheduleTable />
+        <ScheduleForm {...scheduleRows} />
+        {/* <ScheduleTable /> */}
       </div>
     </main>
   );
