ad8a85c93c1bbc5a11ca976a9a5489470960ed3b
unishigure <s.reiuniuni@gmail.com>
2023-09-29 12:23:46 +0900

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { env } from "node:process";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: env.BASE_URL ? new URL(env.BASE_URL) : null,
   title: "gCal CSV Generator",
   description: "Generate CSV for Google Calendar",
+  keywords: ["Google Calender", "CSV", "Tool"],
+  themeColor: "#94a3b8",
+  robots: "index, follow",
+  openGraph: {
+    images: "/image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
